fe6ee21012f4614d4201e99c00d7fb6772dbe918
Елена <tyshenko61@mail.ru>
2020-04-06 11:48:53 +0500

    Подключение к БД вынесено в файл init.php

--- data.php
+++ data.php
@@ -2,13 +2,14 @@
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 $current_user = 1;
+
 /**
  * Получить список проектов текущего пользователя
  * @param  integer $id Идентификатор текущего пользователя
+ * @param  $con Идентификатор соединения с БД
  * @return Ассоциативный массив проектов текущего пользователя
  */
-function get_projects($id) {
-    $con = mysqli_connect("localhost", "root", "", "doings_done");
+function get_projects($id, $con) {
     $sql = "SELECT * FROM projects WHERE id_user = $id";
     $result = mysqli_query($con, $sql);
     return mysqli_fetch_all($result, MYSQLI_ASSOC);
@@ -17,11 +18,11 @@ function get_projects($id) {
  * Получить список задач текущего пользователя
  * @param  integer $id_user Идентификатор текущего пользователя
  * @param  integer $id_project Идентификатор текущего проекта
+ * @param  $con Идентификатор соединения с БД
  * @return Ассоциативный массив задач текущего пользователя
  */
-function get_tasks($id_user, $id_project)
+function get_tasks($id_user, $id_project, $con)
 {
-    $con = mysqli_connect("localhost", "root", "", "doings_done");
     $sql = "SELECT *, name, id_user FROM tasks JOIN projects ON tasks.id_project = projects.id_project WHERE id_user = $id_user";
     if ($id_project) {
         $sql = $sql . " and tasks.id_project= $id_project";
@@ -35,11 +36,12 @@ function get_tasks($id_user, $id_project)
 
 /**
  * Подсчитывает количество задач для проекта
- * @param  nteger $id_project Идентификатор текущего проекта
+ * @param  integer $id_project Идентификатор текущего проекта
+ * @param $con Идентификатор соединения с БД
  * @return integer Количество задач
  */
-function count_tasks($id_project) {
-    $con = mysqli_connect("localhost", "root", "", "doings_done");
+function count_tasks($id_project, $con) {
+ //   $con = mysqli_connect("localhost", "root", "", "doings_done");
     $sql = "SELECT * FROM tasks WHERE tasks.id_project = $id_project";
     $result = mysqli_query($con, $sql);
     return mysqli_num_rows($result);
--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 require_once('helpers.php');
 require_once('data.php');
+require_once('init.php');
 
 $id = $_GET['id'] ?? '';
 
-$page_content = include_template('main.php', ['tasks' => get_tasks($current_user, $id),
-    'projects' => get_projects($current_user), 'show_complete_tasks' => $show_complete_tasks, 'id_project' => $id]);
+$page_content = include_template('main.php', ['tasks' => get_tasks($current_user, $id , $con),
+    'projects' => get_projects($current_user, $con),
+    'show_complete_tasks' => $show_complete_tasks, 'id_project' => $id, 'con' => $con]);
 $layout_content = include_template('layout.php', ['content' => $page_content, 'title' => 'Дела в порядке']);
 
 print($layout_content);
